d4a41ce0c6ce5187a3971773bd1e48bba30d3b9a
aleyate <aleyateve@gmail.com>
2022-08-23 16:16:40 -0500

    Coloque una linea en nueva rama

--- src/main/java/com/aleyateRepaso/inicio.java
+++ src/main/java/com/aleyateRepaso/inicio.java
@@ -5,6 +5,7 @@ public class inicio {
     public static void main(String[] args) {
         System.out.println("Hola developers ");
         System.out.println("Hola developers ");
+        System.out.println("Esta es una linea que esta solo en md movil ");
 
 
     }
